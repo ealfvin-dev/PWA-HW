d19502da583e6ff5e5fca31d17c3d80f48868f73
Erik Alfvin <erikalfvin@gmail.com>
2020-04-24 13:17:11 -0500

    Cached files and intercepted fetch request

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,25 +1,32 @@
-console.log("Hello service-worker world!");
-
 self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open("budget-v1").then((cache) => {
+            console.log("Files cached successfully!");
             return cache.addAll([
+                "/",
                 "./index.html",
                 "./index.js",
                 "./styles.css",
                 "./icons/icon-192x192.png",
-                "./icons/icon-512x512.png",
-                "../models/transaction.js"
+                "./icons/icon-512x512.png"
             ]);
         })
     )
+    self.skipWaiting();
 });
 
 self.addEventListener("fetch", (event) => {
     event.respondWith(
         //try to get resource from cache first, the try the network
         caches.match(event.request).then((response) => {
-            return response || fetch(event.request)
+            return response || fetch(event.request).then((netresp) => {
+                return caches.open("budget-v1").then((cache) => {
+                    cache.put(event.request.url, netresp.clone());
+                    return netresp;
+                })
+            }).catch((err) => {
+                //No network connection - put item in indexed database
+            });
         })
     );
 });
